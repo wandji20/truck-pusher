a298b8f1c4117d9ddd08d52df7f36f6f45a24cfb
wandji20 <wbertrand10@gmail.com>
2024-12-24 22:54:24 +0100

    Handle showing flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+  def flash_alert_class(type)
+    case type.to_s
+    when "success", "notice"
+      "alert-success"
+    when "error", "alert"
+      "alert-danger"
+    else
+      "alert-info"
+    end
+  end
+
+  def flash_icon(type)
+    case type.to_s
+    when "error", "alert"
+      "shared/svgs/exclamation_triangle"
+    when "success", "notice"
+      "shared/svgs/check_circle"
+    else
+      "shared/svgs/information_circle"
+    end
+  end
 end
---
+++ app/javascript/controllers/alert_controller.js
@@ -0,0 +1,13 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  connect() {
+    setTimeout(() => {
+      this.element.remove();
+    }, 5000);
+  }
+
+  removeAlert() {
+    this.element.remove();
+  }
+}
