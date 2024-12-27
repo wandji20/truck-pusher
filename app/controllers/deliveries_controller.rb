d474863eec3a6fe8d777b8be54eb9fc73344c5d4
wandji20 <wbertrand10@gmail.com>
2024-12-27 22:48:37 +0100

    Add datepickr and update deliveries filters to handle search by date

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -1,24 +1,6 @@
 class DeliveriesController < ApplicationController
   def index
-    @deliveries = if params[:type] == "incoming"
-      Delivery.where(destination_id: @current_user.branch_id)
-    elsif params[:type] == "outgoing"
-      Delivery.where(origin_id: @current_user.branch_id)
-    else
-      Delivery.where(origin_id: @current_user.branch_id).or(Delivery.where(destination_id: @current_user.branch_id))
-    end
-
-    @deliveries = @deliveries.joins([ :sender, :receiver ])
-                              .select("deliveries.id, deliveries.agency_id, deliveries.origin_id, deliveries.destination_id,
-                                        deliveries.tracking_number, deliveries.tracking_secret,
-                                        deliveries.status, deliveries.created_at, users.full_name AS sender_name,
-                                        users.telephone AS sender_telephone, receivers_deliveries.full_name AS receiver_name,
-                                        receivers_deliveries.telephone AS receiver_telephone")
-                              .order("deliveries.id": :desc)
-    if params[:q].present?
-      query = Delivery.sanitize_sql_like(params[:q].strip)
-      @deliveries = @deliveries.where("deliveries.tracking_number = ? OR deliveries.tracking_secret = ?", query, query)
-    end
+    search_deliveries
   end
 
   def new
@@ -38,5 +20,37 @@ class DeliveriesController < ApplicationController
     end
 
     set_current_tenant(@current_agency)
+  end
+
+  def search_deliveries
+    @deliveries = if params[:type] == "incoming"
+      Delivery.where(destination_id: @current_user.branch_id)
+    elsif params[:type] == "outgoing"
+      Delivery.where(origin_id: @current_user.branch_id)
+    else
+      Delivery.where(origin_id: @current_user.branch_id).or(Delivery.where(destination_id: @current_user.branch_id))
+    end
+
+    @deliveries = @deliveries.joins([ :sender, :receiver ])
+                              .select("deliveries.id, deliveries.agency_id, deliveries.origin_id, deliveries.destination_id,
+                                        deliveries.tracking_number, deliveries.tracking_secret,
+                                        deliveries.status, deliveries.created_at, users.full_name AS sender_name,
+                                        users.telephone AS sender_telephone, receivers_deliveries.full_name AS receiver_name,
+                                        receivers_deliveries.telephone AS receiver_telephone")
+                              .order("deliveries.id": :desc)
+    if params[:q].present?
+      query = Delivery.sanitize_sql_like(params[:q].strip)
+      @deliveries = @deliveries.where("deliveries.tracking_number = ? OR deliveries.tracking_secret = ?", query, query)
+    end
+
+    if params[:from].present?
+      from = DateTime.parse(params[:from])
+      @deliveries = @deliveries.where("deliveries.created_at >= ?", from)
+    end
+
+    if params[:to].present?
+      to = DateTime.parse(params[:to])
+      @deliveries = @deliveries.where("deliveries.created_at <= ?", to)
+    end
   end
 end
