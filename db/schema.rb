61d27703c7c6c1276b0f416dcd6bae6ffe8a78f6
wandji20 <wbertrand10@gmail.com>
2025-01-11 01:13:23 +0100

    Switch database to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.0].define(version: 2024_12_25_003830) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
+
   create_table "agencies", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
