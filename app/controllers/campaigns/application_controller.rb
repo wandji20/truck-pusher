61ec6d5e06d2e2b1c8a11287304c60f35308b1c2
wandji20 <wbertrand10@gmail.com>
2025-01-13 01:16:18 +0100

    Add marketer invitation controller and views

---
+++ app/controllers/campaigns/application_controller.rb
@@ -0,0 +1,14 @@
+module Campaigns
+  class ApplicationController < ActionController::Base
+    before_action :require_admin
+
+    layout "campaigns"
+
+    private
+
+    def require_admin
+      http_basic_authenticate_or_request_with name: Rails.application.credentials["app_username"] || "",
+                                  password: Rails.application.credentials["app_password"] || ""
+    end
+  end
+end
