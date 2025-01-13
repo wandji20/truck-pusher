1622cbf78c9a3bfbd9ae49a67400ee91f13858c2
wandji20 <wbertrand10@gmail.com>
2025-01-13 01:18:17 +0100

    Add logic for markerter login and login screen

--- app/controllers/campaigns/application_controller.rb
+++ app/controllers/campaigns/application_controller.rb
@@ -1,6 +1,7 @@
 module Campaigns
   class ApplicationController < ActionController::Base
     before_action :require_admin
+    include MarketerAuthentication
 
     layout "campaigns"
 
---
+++ app/controllers/concerns/marketer_authentication.rb
@@ -0,0 +1,55 @@
+module MarketerAuthentication
+  extend ActiveSupport::Concern
+
+  included do
+    before_action :require_authentication
+    helper_method :authenticated?
+  end
+
+  class_methods do
+    def allow_unauthenticated_access(**options)
+      skip_before_action :require_authentication, **options
+    end
+  end
+
+  private
+
+  def authenticated?
+    resume_session
+  end
+
+  def require_authentication
+    resume_session || request_authentication
+  end
+
+  def resume_session
+    Current.session ||= find_session_by_cookie
+  end
+
+  def find_session_by_cookie
+    Session.find_by(id: cookies.signed[:session_id]) if cookies.signed[:session_id]
+  end
+
+  def request_authentication
+    session[:return_to_after_authenticating] = request.url
+
+    flash[:error] = t("campaigns.sessions.login_required")
+    redirect_to campaigns_login_path
+  end
+
+  def after_authentication_url
+    session.delete(:return_to_after_authenticating) || campaigns_merchants_path
+  end
+
+  def start_new_session_for(marketer)
+    marketer.sessions.create!(user_agent: request.user_agent, ip_address: request.remote_ip).tap do |session|
+      Current.session = session
+      cookies.signed.permanent[:session_id] = { value: session.id, httponly: true, same_site: :lax }
+    end
+  end
+
+  def terminate_session
+    Current.session.destroy
+    cookies.delete(:session_id)
+  end
+end
